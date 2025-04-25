8304b31b9f82ef8c9f06f33f7ff72b95012c2c92
Khushi Gupta <khushigupttaa18@gmail.com>
2025-04-25 23:33:10 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { ThemeProvider } from './ThemeContext';
-import { Web3Provider } from './Web3Context';
+import { ThemeProvider } from './ThemeContext'; // Assuming this exists
+import { Web3Provider } from './Web3Context';     // Assuming this exists
+import { WatchlistProvider } from './context/WatchlistContext'; // Import the new provider
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <Web3Provider>
-        <App />
+        <WatchlistProvider>
+          <App />
+        </WatchlistProvider>
       </Web3Provider>
     </ThemeProvider>
   </StrictMode>
